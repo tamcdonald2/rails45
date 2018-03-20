62b4585a3da26e1324643b3e90ab894e9d5b9729
Trinity M <tamcdonald2@cps.edu>
2018-03-20 13:27:32 +0000

    Install postgres and update database.yml

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+    get 'todo/create', to: 'todo#create'
+    get 'todo/new', to: "todo#new"
 get 'todo/index', to: 'todo#index'
 
 get 'todo/show/:id', to: 'todo#show'
